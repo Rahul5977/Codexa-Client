57e7eff0cb62051f98d7081c2fe6e34630e4f151
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2026-01-30 11:56:37 +0530

    fix(site-header): add sticky positioning and backdrop blur to header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -25,7 +25,7 @@ export function SiteHeader() {
 
   return (
     <>
-      <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) sticky top-0 z-30 bg-background/40 backdrop-blur supports-backdrop-filter:backdrop-filter-[blur(10px)]">
         <div className="flex w-full items-center gap-1 px-4 py-3 lg:gap-2 lg:px-6">
           <SidebarTrigger className="-ml-1" />
           <Separator
